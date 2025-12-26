1b10b3ffc8da2f4f83981be46e0467c64e5ff75c
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2025-12-26 17:36:34 +0530

    adding auth

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>vision.ai</Text>
     </View>
   );
 }
